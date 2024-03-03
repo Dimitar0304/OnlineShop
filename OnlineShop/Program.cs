399022d9b4aba1f46951f2c2bf37ca18a0d8eac7
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2024-03-03 21:25:31 +0200

    Some Layout changes!

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -40,7 +40,7 @@ builder.Services.ConfigureApplicationCookie(o =>
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
-    options.SignIn.RequireConfirmedAccount = true;
+    options.SignIn.RequireConfirmedAccount = false;
     options.Password.RequireDigit = false;
     options.Password.RequireUppercase = false;
     options.Password.RequireNonAlphanumeric = false;
