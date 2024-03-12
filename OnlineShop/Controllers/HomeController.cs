3907f534c3faa252033715016ccfd20723ee6c08
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2024-03-12 19:24:38 +0200

    Add some CSS to home page partial view allcontroller

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -28,5 +28,12 @@ namespace OnlineShop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public ActionResult RenderSearchBar()
+        {
+            return PartialView("_ShowSearchFormPartial");
+        }
+
     }
 }
