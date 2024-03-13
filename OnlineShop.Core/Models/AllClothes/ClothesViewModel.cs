1f260ff7b512ba3a85285767ccdde0ed94ee635f
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2024-03-13 12:36:20 +0200

    Works to search bar and display matches with this

---
+++ OnlineShop.Core/Models/AllClothes/ClothesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Core.Models.AllClothes
+{
+    //class that is for each clothes in shop
+    public class ClothesViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string ImageUrl { get; set; }=null!;
+        public decimal Price { get; set; }
+    }
+}
--- OnlineShop/Controllers/AllClothesController.cs
+++ OnlineShop/Controllers/AllClothesController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Core.Services.Contracts;
+using OnlineShop.Services.Contracts;
 
 namespace OnlineShop.Controllers
 {
     public class AllClothesController : Controller
     {
-        public IActionResult Index()
+        private readonly IGarmentService garmentService;
+        private readonly IShoeService shoeService;
+        public AllClothesController(IGarmentService _garmentService,IShoeService _shoeService)
         {
-            return View();
+            garmentService = _garmentService;
+            shoeService = _shoeService;
+        }
+        public IActionResult SearchAllByName(string productName)
+        {
+            var garments = garmentService.GetAllGarmentsAsync().Result
+                .Where(g=>g.Name.Contains(productName.ToLower()));
+
+            var shoes = shoeService.GetAllShoeAsync().Result
+               .Where(g => g.Name.Contains(productName.ToLower()));
+
+
         }
     }
 }
