afd068baf5886e986152acf0d7550a28e1e389e1
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2024-03-30 20:37:34 +0200

    I can't test the app:(

---
+++ OnlineShop.Tests/Controllers/GarmentControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnlineShop.Controllers;
+using OnlineShop.Core.Services.Contracts;
+using OnlineShop.Services.Contracts;
+using OnlineShop.Services.GarmentService;
+
+namespace OnlineShop.Tests.Controllers
+{
+    [TestFixture]
+    public class GarmentControllerTests
+    {
+        
+        private readonly Mock<IGarmentService> service=new Mock<IGarmentService>();
+        private readonly Mock<IGarmentSizeService> sizeService = new Mock<IGarmentSizeService>();
+                
+        
+
+        [Test]
+        public void AllMethodShouldReturnPageWithGarments()
+        {
+            //arange 
+            var controller = new GarmentController(service.Object, sizeService.Object);
+
+            //act 
+             var result = controller.All(1).Result;
+
+            //assert
+            Assert.That(result, Is.Positive);
+
+        }
+        
+        
+
+    }
+}
