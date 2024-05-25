650fe200dd445de3b4282c11efc473f4dccc172f
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2024-05-25 17:57:55 +0300

    Add promotion data entity

---
+++ OnlineShop.Infrastructure/Data/Models/Promotion.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Infrastructure.Data.Models
+{
+    /// <summary>
+    /// Promotion data entity
+    /// </summary>
+    public class Promotion
+    {
+        /// <summary>
+        /// Promotion identifier
+        /// </summary>
+        [Key]
+        [Comment("Promotion identifier")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Promotion name
+        /// </summary>
+        [Required]
+        [Comment("Promotion name")]
+        public string PromoName { get; set;} = null!;
+
+        /// <summary>
+        /// Promotion start date
+        /// </summary>
+        [Required]
+        [Comment("Promotion start date")]
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// Promotion end date
+        /// </summary>
+        [Required]
+        [Comment("Promotion end date")]
+        public DateTime End { get; set; }
+
+        /// <summary>
+        /// Promotion code
+        /// </summary>
+        [Required]
+        [Comment("Promotion code")]
+        public string PromoCode { get; set; } = null!;
+
+        /// <summary>
+        /// Promotion procentage
+        /// </summary>
+        [Required]
+        [Comment("Promotion procentage")]
+        public int PromotionProcentage { get; set; }
+    }
+}
