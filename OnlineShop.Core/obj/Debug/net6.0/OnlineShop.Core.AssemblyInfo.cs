8674c347ff14f68e8e10d5c1e60fbd459e73bfe4
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2024-05-31 17:57:19 +0300

    some modifies

--- OnlineShop.Core/obj/Debug/net6.0/OnlineShop.Core.AssemblyInfo.cs
+++ OnlineShop.Core/obj/Debug/net6.0/OnlineShop.Core.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShop.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b92ae336d64eea1e9adbe724ec3317415a2b658")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShop.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShop.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OnlineShop/obj/Debug/net6.0/OnlineShop.AssemblyInfo.cs
+++ OnlineShop/obj/Debug/net6.0/OnlineShop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b92ae336d64eea1e9adbe724ec3317415a2b658")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
